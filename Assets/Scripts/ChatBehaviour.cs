50c6d7cc744e94134f6ec29db0f886d21451cfd9
Thanathip Sukkulcharoen <thanathip.sukkulcharoen@gmail.com>
2020-07-26 21:16:35 +0700

    Add Chat System - Client can send message to all clients - Host cannot send message to other clients

---
+++ Assets/Scripts/ChatBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+using System;
+
+public class ChatBehaviour : NetworkBehaviour
+{
+    [SerializeField] private GameObject chatUI = null;
+    [SerializeField] private TMP_Text chatText = null;
+    [SerializeField] private TMP_InputField inputField = null;
+
+    private static event Action<TMP_Text,string> OnMessage;
+
+    public override void OnStartAuthority()
+    {
+        chatUI.SetActive(true);
+
+        OnMessage += HandleNewMessage;
+    }
+
+    [ClientCallback]
+    private void OnDestroy()
+    {
+        if (!hasAuthority) { return; }
+        OnMessage -= HandleNewMessage;
+    }
+
+    private void HandleNewMessage(TMP_Text sender, string message)
+    {
+        sender.text = message;
+    }
+
+    [Client]
+    public void Send(string message)
+    {
+        if (!Input.GetKeyDown(KeyCode.Return)) { return; }
+        if (string.IsNullOrWhiteSpace(message)) { return; }
+        CmdSendMessage(inputField.text);
+        inputField.text = string.Empty;
+    }
+
+    [Command]
+    private void CmdSendMessage(string message)
+    {
+        RpcHandleMessage($"{message}");
+    }
+
+    [ClientRpc]
+    private void RpcHandleMessage(string message)
+    {
+        OnMessage?.Invoke(chatText,$"{message}");
+    }
+}
